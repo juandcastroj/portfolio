0986e6ea02fe571b7e368c7a9f4553b65c25ac67
Juan David Castro <juandavid4130@gmail.com>
2025-02-20 11:39:14 -0500

    Adding test api results to local to avoid unnecesary calls

--- src/components/searchMovies.jsx
+++ src/components/searchMovies.jsx
@@ -1,17 +1,18 @@
 export function SearchMovie(params) {
-    
-
-    
 
     return(
         <>
-            <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
-                <div className="p-20 text-center">
-                    <h1 className="text-green-600 dark:text-gray-200 font-bold text-3xl mb-2">Search Movies</h1>
-                    <input className="mx-4 p-2 rounded-md" placeholder="insert movie" />
-                    <button className="text-gray-200 dark:text-gray-700 p-2 bg-blue-700 dark:bg-gray-200 rounded-md">Search</button>
-                </div>
-            </div>
+            <header className="pt-20 flex flex-col justify-center text-center">
+                <h1 className="text-green-600 dark:text-gray-200 font-bold text-3xl mb-2">Search Movies</h1>
+
+                <form className="p-20 text-center">
+                    <input type="text" className="mx-4 p-2 rounded-md" placeholder="insert movie" />
+                    <button type="submit" className="text-gray-200 dark:text-gray-700 p-2 bg-blue-700 dark:bg-gray-200 rounded-md">Search</button>
+                </form>
+            </header>
+            <main>
+                <h1 className="text-center text-white text-4xl">movies will be shown here</h1>
+            </main>
         </>
     )
 }
