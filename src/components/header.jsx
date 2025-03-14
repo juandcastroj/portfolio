68df4680d0abe9a8b41827691eab885f4476155f
Juan David Castro <juandavid4130@gmail.com>
2025-03-13 19:57:12 -0500

    Simplifying and isolating navbar and mobile navbar code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 import ThemeSwitcher from './themeSwitcher'
 import { Link } from 'react-router-dom'
+import { MobileHeader } from './mobileHeader'
 
 const navigation = [
     { name: 'Home', href: '/' },
@@ -21,8 +21,6 @@ export default function Header() {
 
     return ( 
         <header className="fixed inset-x-0 top-0 z-50">
-
-            {/* Desktop navbar */}
             <nav aria-label="Global" className="animate-fade-down animate-duration-[3000ms] animate-delay-200 mx-auto flex max-w-7xl items-center justify-end lg:justify-center p-6 lg:px-8">
                 <div className="flex lg:hidden fixed top-6 right-6">
                     <button
@@ -42,58 +40,11 @@ export default function Header() {
                         </Link>
                     ))}
                 </div>
-               
+
                 <ThemeSwitcher className="hidden lg:inline"/>
             </nav>
-            
 
-                {/* Mobile Navbar */}
-            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                <div className="fixed inset-0 z-50" />
-                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-[#2b0076] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-
-                    <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
-                        <img
-                            alt=""
-                            src="https://tailwindui.com/plus/img/logos/mark.svg?color=blue&shade=300"
-                            className="h-8 w-auto"
-                        />
-                        </a>
-
-                        <button
-                        type="button"
-                        onClick={handleMobileMenu}
-                        className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-gray-200"
-                        >
-                        <span className="sr-only">Close menu</span>
-                        <XMarkIcon aria-hidden="true" className="size-6" />
-                        </button>
-                    </div>
-
-                    <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
-
-                            <div className="space-y-2 py-6">
-                                {navigation.map((item) => (
-                                <a
-                                    onClick={() => setMobileMenuOpen(false)}
-                                    key={item.name}
-                                    href={item.href}
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 dark:text-blue-200 hover:bg-gray-50"
-                                >
-                                    {item.name}
-                                </a>
-                                ))}
-                            </div>
-                            <ThemeSwitcher className="inline lg:hidden"/>
-                        </div>
-                    </div>
-
-                </DialogPanel>
-            </Dialog>
-
+            <MobileHeader navigation={navigation} handleMobileMenu={handleMobileMenu} mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
         </header>
-     );
+    );
 }
---
+++ src/components/mobileHeader.jsx
@@ -0,0 +1,44 @@
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import ThemeSwitcher from './themeSwitcher'
+
+export function MobileHeader( { navigation, mobileMenuOpen, setMobileMenuOpen, handleMobileMenu }) {
+
+    return (
+        <>
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-50" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white dark:bg-[#2b0076] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center right-6 top-6 absolute">
+                        <button
+                        type="button"
+                        onClick={handleMobileMenu}
+                        className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-gray-200"
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon aria-hidden="true" className="size-6" />
+                        </button>
+                    </div>
+
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 dark:text-blue-200 hover:bg-gray-50"
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+
+                            <ThemeSwitcher className="inline lg:hidden"/>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
+        </>
+    )
+}
