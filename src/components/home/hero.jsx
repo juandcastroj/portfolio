2d379bd1e862ca5b3bc583a12da6777e12899d0f
Juan David Castro <juandavid4130@gmail.com>
2025-06-03 21:15:19 -0500

    color on theme switcher

--- src/components/home/hero.jsx
+++ src/components/home/hero.jsx
@@ -6,8 +6,9 @@ export default function Hero() {
   return (
     <div className="relative isolate -z-10 overflow-hidden pt-14 animate-fade-down animate-duration-[3000ms] animate-delay-800">
       <div className="mx-auto max-w-5xl px-6 py-12 sm:py-28 lg:py-36 lg:px-8">
-        <div className="mx-auto max-w-4xl lg:mx-0 lg:flex gap-4 lg:max-w-none">
+        <div className="mx-auto max-w-4xl lg:mx-0 lg:flex gap-12 lg:max-w-none">
           <div className="flex flex-col justify-between">
+
               <h2 className="max-w-2xl text-balance text-3xl sm:text-4xl font-semibold tracking-tight text-green-900 dark:text-white lg:text-7xl lg:col-span-2 xl:col-auto">
                 Hi. I&#39;m Juan David.
               </h2>
@@ -21,16 +22,17 @@ export default function Hero() {
                 </p>
               </div>
           </div>
-            <img
-                alt="JD photo"
-                src={JD_photoDark}
-                className="hidden dark:block aspect-square w-full max-w-xl rounded-full object-cover lg:max-w-72 text-center"
-              />
-            <img
-                alt="JD photo"
-                src={JD_photoLight}
-                className="dark:hidden aspect-square w-full max-w-xl rounded-full object-cover lg:max-w-72 text-center"
-              />
+
+          <img
+              alt="JD photo"
+              src={JD_photoDark}
+              className="hidden dark:block aspect-square w-full max-w-xl rounded-full object-cover lg:max-w-72 text-center"
+            />
+          <img
+              alt="JD photo"
+              src={JD_photoLight}
+              className="dark:hidden aspect-square w-full max-w-xl rounded-full object-cover lg:max-w-72 text-center"
+            /> 
         </div>
       </div>
     </div>
