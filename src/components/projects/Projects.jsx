5d660dd9e61d690617a18623f9abcded3e4d167b
Juan David Castro <juandavid4130@gmail.com>
2025-07-28 00:44:04 -0500

    Updating projects info

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,14 +13,14 @@ export default function Projects() {
             </p>
         </div>
 
-
         <ul role="list" className="space-y-8 xl:col-span-3 animate-fade-down animate-duration-[1500ms] animate-delay-500">
           {  projectsData.map(
             ({ company, role, imageUrl, description, url, repo }, index) => (
               <li
                 key={index}
-                className="flex flex-col gap-4 lg:gap-10 lg:flex-row p-3 md:pr-12 cursor-pointer
-                           border-[1px] dark:border-0 rounded-3xl hover:shadow-md bg-white/10 hover:bg-white/30 dark:hover:bg-white/20 hover:backdrop-blur-md" >
+                className="flex flex-col gap-4 lg:gap-10 lg:flex-row p-3 md:pr-12
+                           border-[1px] dark:border-0 rounded-3xl hover:shadow-md 
+                           bg-white/10 hover:bg-white/30 dark:hover:bg-white/20 hover:backdrop-blur-md" >
               {/* <iframe src={url} className="aspect-square w-[24rem] md:w-96 xl:w-[40rem] h-80 flex-none rounded-none md:rounded-2xl object-cover"></iframe> */}
                   <img
                     alt={company}
@@ -28,6 +28,8 @@ export default function Projects() {
                     className="w-96 xl:w-[36rem] rounded-lg md:rounded-2xl"
                   />
                 <div className="max-w-xl flex-auto pt-2 lg:pt-6 text-justify">
+
+
                   <a href={url} target="_blank" rel="noopener noreferrer">
                     <h3 className="text-xl font-semibold tracking-tight text-green-900 dark:text-blue-300">
                       {company}
@@ -39,24 +41,50 @@ export default function Projects() {
                       {description}
                     </p>
                   </a>
+
+
+            <div className="flex items-center gap-4 mt-4 flex-row">
+              
                   <a
+                      href={url}
+                      className="text-green-900 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
+                    >
+                    <span className="sr-only">Repository</span>
+                      <svg
+                        fill="currentColor"
+                        viewBox="0 0 30 30"
+                        aria-hidden="true"
+                        className="mt-4 size-10"
+                      >
+                        <path
+                          d="M14.831,17c-.256,0-.512-.098-.707-.293-.391-.391-.391-1.023,0-1.414l2.583-2.583c.188-.189,.293-.44,.293-.708s-.104-.518-.293-.707l-2.583-2.584c-.391-.391-.391-1.024,0-1.414,.391-.391,1.023-.391,1.414,0l2.583,2.583c.566,.566,.879,1.32,.879,2.121s-.312,1.555-.879,2.122l-2.583,2.583c-.195,.195-.451,.293-.707,.293Zm-4.955-.298c.391-.391,.391-1.023,0-1.414l-2.583-2.583c-.189-.189-.293-.44-.293-.708s.104-.518,.293-.707l2.583-2.583c.391-.391,.391-1.023,0-1.414s-1.023-.391-1.414,0l-2.583,2.583c-.567,.567-.879,1.32-.879,2.122s.312,1.555,.879,2.122l2.583,2.583c.195,.195,.451,.293,.707,.293s.512-.098,.707-.293Zm14.124,2.298V5c0-2.757-2.243-5-5-5H5C2.243,0,0,2.243,0,5v14c0,2.757,2.243,5,5,5h14c2.757,0,5-2.243,5-5ZM19,2c1.654,0,3,1.346,3,3v14c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V5c0-1.654,1.346-3,3-3h14Z"
+                          clipRule="evenodd"
+                          fillRule="evenodd"
+                        />
+                      </svg>
+                  </a>
+
+                 <a
                     href={repo}
                     className="text-green-900 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
                   >
-                    <span className="sr-only">Repository</span>
-                    <svg
-                      fill="currentColor"
-                      viewBox="0 0 30 30"
-                      aria-hidden="true"
-                      className="mt-4 size-10"
-                    >
-                      <path
-                        d="M14.831,17c-.256,0-.512-.098-.707-.293-.391-.391-.391-1.023,0-1.414l2.583-2.583c.188-.189,.293-.44,.293-.708s-.104-.518-.293-.707l-2.583-2.584c-.391-.391-.391-1.024,0-1.414,.391-.391,1.023-.391,1.414,0l2.583,2.583c.566,.566,.879,1.32,.879,2.121s-.312,1.555-.879,2.122l-2.583,2.583c-.195,.195-.451,.293-.707,.293Zm-4.955-.298c.391-.391,.391-1.023,0-1.414l-2.583-2.583c-.189-.189-.293-.44-.293-.708s.104-.518,.293-.707l2.583-2.583c.391-.391,.391-1.023,0-1.414s-1.023-.391-1.414,0l-2.583,2.583c-.567,.567-.879,1.32-.879,2.122s.312,1.555,.879,2.122l2.583,2.583c.195,.195,.451,.293,.707,.293s.512-.098,.707-.293Zm14.124,2.298V5c0-2.757-2.243-5-5-5H5C2.243,0,0,2.243,0,5v14c0,2.757,2.243,5,5,5h14c2.757,0,5-2.243,5-5ZM19,2c1.654,0,3,1.346,3,3v14c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V5c0-1.654,1.346-3,3-3h14Z"
-                        clipRule="evenodd"
-                        fillRule="evenodd"
-                      />
-                    </svg>
+                    <span className="sr-only">Deploy</span>
+                     <svg
+                        fill="currentColor"
+                        viewBox="0 0 30 30"
+                        aria-hidden="true"
+                        className="mt-4 size-10"
+                      >
+                        <path
+                          d="M14.831,17c-.256,0-.512-.098-.707-.293-.391-.391-.391-1.023,0-1.414l2.583-2.583c.188-.189,.293-.44,.293-.708s-.104-.518-.293-.707l-2.583-2.584c-.391-.391-.391-1.024,0-1.414,.391-.391,1.023-.391,1.414,0l2.583,2.583c.566,.566,.879,1.32,.879,2.121s-.312,1.555-.879,2.122l-2.583,2.583c-.195,.195-.451,.293-.707,.293Zm-4.955-.298c.391-.391,.391-1.023,0-1.414l-2.583-2.583c-.189-.189-.293-.44-.293-.708s.104-.518,.293-.707l2.583-2.583c.391-.391,.391-1.023,0-1.414s-1.023-.391-1.414,0l-2.583,2.583c-.567,.567-.879,1.32-.879,2.122s.312,1.555,.879,2.122l2.583,2.583c.195,.195,.451,.293,.707,.293s.512-.098,.707-.293Zm14.124,2.298V5c0-2.757-2.243-5-5-5H5C2.243,0,0,2.243,0,5v14c0,2.757,2.243,5,5,5h14c2.757,0,5-2.243,5-5ZM19,2c1.654,0,3,1.346,3,3v14c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V5c0-1.654,1.346-3,3-3h14Z"
+                          clipRule="evenodd"
+                          fillRule="evenodd"
+                        />
+                      </svg>
                   </a>
+            </div>
+
+
                 </div>
               </li>
             ))
--- src/constants/projectsPortfolio.js
+++ src/constants/projectsPortfolio.js
@@ -5,13 +5,14 @@ import SiinHero from '../assets/images/projects/Siin_hero.jpeg';
 import TahsinHero from '../assets/images/projects/Tahsin_hero.jpeg';
 import CoffeeStrapHero from '../assets/images/projects/CoffeeStrap.jpeg';
 
+const CoffeeStrapUrl = 'coffeestrap.com';
 
 export const projectsData = [
     {
       company: 'Nuestratierraco',
-      role: 'Full stack developer',
+      role: 'Web Developer',
       imageUrl: NuestraTierraHero,
-      description: 'Complete landing page with ecommerce section full developed by me for a non-profit corporation. Using React js, vite and Ecwid for products manage.',
+      description: 'Web application with ecommerce section full developed by me for a Colombian environmental ONG. Using React js, Firebase for autentication and customer manage, such as Ecwid for store and products manage.',
       url: 'https://nuestratierraco.netlify.app/',
       repo: '#'    
     },
@@ -19,7 +20,7 @@ export const projectsData = [
       company: 'Interkey',
       role: 'Designer and Frontend developer',
       imageUrl: InterkeyHero,
-      description: 'Complete landing page design proposed and developed by me for a client in Arabic and English languages, using HTML, CSS, and vanilla JavaScript. Has a contact form and a blog section.',
+      description: 'Complete landing page design proposed and developed by me for a client in Arabic and English languages, using HTML, CSS, and vanilla JavaScript for the MVP. Includes a contact form and a blog section.',
       url: 'https://interkey2023.netlify.app/',
       repo: '#'    
     },
@@ -27,7 +28,7 @@ export const projectsData = [
       company: 'SessionForward Inc',
       role: 'Web developer',
       imageUrl: SessionForwardHero,
-      description: 'ABtesting web service application that has my work on different dashboard components and some complete sections (onboarding, profile, etc), using HTML, CSS, JavaScript and Vue js framework. Node js for backend and Postgresql for database.',
+      description: 'ABtesting web service application that has my work on different components and some complete sections (onboarding, profile, etc), using HTML, CSS, JavaScript and my first experience with Vue js framework in the dashboard. Node js for backend and Postgresql for database.',
       url: 'https://www.sessionforward.com/',
       repo: '#'    
     },
@@ -35,15 +36,15 @@ export const projectsData = [
       company: 'Siin shop',
       role: 'Frontend developer',
       imageUrl: SiinHero,
-      description: 'Ecommerce landing Developed following mockup design guidelines and high site interactivity specifications. It features Arabic and English languages, as well as a blog section and contact form.',
-      url: 'https://gorgeous-cucurucho-749274.netlify.app/',
+      description: 'Developed some sections (all previous main landing section and actual contact form, privacy policy and terms) following mockup design guidelines and high level interactivity specifications such as advanced animations. It features Arabic and English languages.',
+      url: 'https://siin.shop/en',
       repo: '#'
     },
     {
       company: 'Tahsin Alwajiha',
       role: 'Frontend developer',
       imageUrl: TahsinHero,
-      description: 'Designed and developed a demo website for a promising arabic company on 2022. This site is fully responsive and has light and dark theme. It was developed using HTML, CSS, and JavaScript.',
+      description: 'Designed and developed a demo website landing for a promising arabic company on 2022. Fully responsive and has light and dark colours theme. It was developed using HTML, CSS, and JavaScript.',
       url: 'https://tahsinalwajiha.com/',
       repo: '#'
     },
@@ -51,7 +52,7 @@ export const projectsData = [
       company: 'CoffeeStrap',
       role: 'Mobile developer',
       imageUrl: CoffeeStrapHero,
-      description: 'Mobile version cross platform (Android and IOS) for an existing languages web platform called coffeeStrap, published on Google Play and Appstore, built with Flutter framework.',
+      description: 'Mobile version cross platform (Android and IOS) for the languages web platform: ' + CoffeeStrapUrl + ' published on Google Play Appstore, built with Flutter framework and Node js.',
       url: 'https://play.google.com/store/apps/details?id=com.coffeestrap.coffeestrap&hl=en&gl=US',
       repo: '#'
     },
