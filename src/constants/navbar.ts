c35f03841d25538675cbbbce62257c277ceb2fd2
Juan David Castro <juandavid4130@gmail.com>
2026-03-03 11:11:31 -0500

    Installing typescript

---
+++ src/constants/navbar.ts
@@ -0,0 +1,11 @@
+type NavBarItem = {
+  name: string
+  to: string
+}
+
+export const navBarItems: NavBarItem[] = [
+  { name: 'About JD', to: '/' },
+  { name: 'Projects', to: '/projects' },
+  { name: 'Stack', to: '/stack' },
+  { name: 'Contact', to: '/contact' },
+]
